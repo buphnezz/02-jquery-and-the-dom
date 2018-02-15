46a92362c6de256d45e6b9487da601a08e4a20a9
haronYunis <haronyunis@icloud.com>
2018-02-14 19:11:11 -0800

    articles are now showing with correct infromation

--- haron-zachary/scripts/article.js
+++ haron-zachary/scripts/article.js
@@ -5,7 +5,7 @@ let articles = [];
 // COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
 // PUT YOUR RESPONSE HERE
 
-function Article (rawDataObj) {
+function Article(rawDataObj) {
   this.title = rawDataObj.title;
   this.category = rawDataObj.category;
   this.author = rawDataObj.author;
@@ -17,16 +17,14 @@ function Article (rawDataObj) {
   // Save ALL the properties of `rawDataObj` into `this`
 }
 
-Article.prototype.toHtml = function() {
+Article.prototype.toHtml = function () {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
   // PUT YOUR RESPONSE HERE
-$('p').append('name', 'this.author');
 
   let $newArticle = $('article.template').clone();
-  $newArticle.append('something');
-  console.log($newArticle);
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
-
+  
+  $newArticle.removeClass('template');
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
 
@@ -37,29 +35,30 @@ $('p').append('name', 'this.author');
       3. article title,
       4. article body, and
       5. publication date. */
-  $(document).ready(function () {
-    $("body").append(this.articles[0]);
-    });
-    
+
+
+  $newArticle.find('.by-line a').html(this.author);
+  $newArticle.find('.by-line a').attr('href', this.authorUrl);
+  $newArticle.find('h1:first').html(this.title);
+  $newArticle.find('.article-body').html(this.body);
   // REVIEW: Display the date as a relative number of 'days ago'
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
   $newArticle.append('<hr>');
   return $newArticle;
 };
 
-rawData.sort(function(a,b) {
+rawData.sort(function (a, b) {
   // REVIEW: Take a look at this sort method; This may be the first time we've seen it. Look at the docs and think about how the dates would be sorted if the callback were not included in this method.
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
 // TODO: Refactor these for loops using the .forEach() array method.
 
-for(let i = 0; i < rawData.length; i++) {
-  articles.push(new Article(rawData[i]));
-}
+rawData.forEach(function(index) {
+  articles.push(new Article(index));
+});
 
-// for(let i = 0; i < articles.length; i++) {
-//   $('#articles').append(articles[i].toHtml());
-// }
+articles.forEach(function(article) {
+  $('#articles').append(article.toHtml());
+});
 
-$('#articles').append(articles[0].toHtml());
